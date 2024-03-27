cd23bd82e0ec400eee340822b88e5cfe401a63d8
Emanuel Coletti <emanuelcoletti001@gmail.com>
2024-03-26 21:28:01 -0300

    mejorando el contenido y agregando logica al js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,28 @@ function copyEmail() {
   emailTextarea.select();
   document.execCommand("copy");
 }
+
+/*cambiador de color de texto logica*/
+
+document.addEventListener("DOMContentLoaded", function () {
+  const iconos = document.querySelectorAll(".iconos i");
+
+  function cambiarColorIcono() {
+    iconos.forEach((icono) => {
+      const colorRandom = getRandomColor();
+      icono.style.transition = "color 1s linear"; // Aplica la transición suave de color
+      icono.style.color = colorRandom;
+    });
+  }
+
+  function getRandomColor() {
+    const letras = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letras[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  setInterval(cambiarColorIcono, 1000); // Cambia el color cada segundo
+});
